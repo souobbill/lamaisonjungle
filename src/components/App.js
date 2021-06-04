28d4337a59ec56314c1242a63350f633761b78d2
souobbill <bsouob@gmail.com>
2021-06-04 14:58:20 +0100

    jungle

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React          from 'react';
+
+import '../App.css';
+import Cart from '../components/Cart'
+import Banner from '../components/Banner';
+
+function App() {
+  return <div className='App'>
+            <Banner/>
+            <Cart/>
+          </div> 
+}
+
+export default (App);
